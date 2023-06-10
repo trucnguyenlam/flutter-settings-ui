add957bb35c906423595e93cd9a364fc9bddb75d
Truc Nguyen Lam <trucnguyenlam@gmail.com>
2023-06-10 09:06:01 +0100

    Null safety

--- lib/src/settings_section.dart
+++ lib/src/settings_section.dart
@@ -81,7 +81,7 @@ class SettingsSection extends AbstractSection {
             title!,
             style: titleTextStyle ??
                 TextStyle(
-                  color: Theme.of(context).accentColor,
+                  color: Theme.of(context).colorScheme.secondary,
                   fontWeight: FontWeight.bold,
                 ),
             maxLines: maxLines,
